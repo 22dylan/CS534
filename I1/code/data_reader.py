46ad7b9accfac95c4f05613f038cf0e6126960ea
Meredith Leung <leungmer@oregonstate.edu>
2019-10-17 14:02:43 -0700

    Add files via upload
    made normalization changes

--- I1/code/data_reader.py
+++ I1/code/data_reader.py
@@ -1,21 +1,23 @@
-
+import numpy as np
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-def data_reader(path, norm):
+def data_reader(path, norm, normval=None):
     p=0 #change to something else to not create plots of input data
     df = pd.read_csv(path)
     df = df.drop('id',axis=1) #remove id column
 #    
-#    df['year']=pd.DateTimeIndex(df['date']).year #create day mo yr columns
-#    df['month']=pd.DateTimeIndex(df['date']).month
-#    df['day']=pd.DateTimeIndex(df['date']).day
-##        
+    df['year']=pd.DatetimeIndex(df['date']).year #create day mo yr columns
+    df['month']=pd.DatetimeIndex(df['date']).month
+    df['day']=pd.DatetimeIndex(df['date']).day
+        
+    df = df.drop('date',axis=1) #remove date column
+
     ############## wasn't sure if zipcode/lat/long counted as a feature what do you guys think?#######
-    features=['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront',
+    features=['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront',\
               'view','condition','grade','sqft_above','sqft_basement','yr_built',\
-              'yr_renovated','sqft_living15','sqft_lot15']
+              'yr_renovated','zipcode', 'lat','long','sqft_living15','sqft_lot15','price']
     num_features=['waterfront','condition','grade']
       
     ####for statistics of features###
@@ -31,10 +33,14 @@ def data_reader(path, norm):
     #for feat in num_features:
     #    pctwith=df[feat]    
     
-    #normalize data to range of 0 to 1
+    #normalize data to range of 0 to 
+    
     if norm==True:
+        if normval is None:
+            normval=df/df.max()
         for feat in features:
-            df[feat]=df[feat]/df[feat].max()
-
+            df[feat]=df[feat]/normval[feat]
+                
     data=df.to_numpy()
-    return data
+
+    return data, normval
