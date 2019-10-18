4493fa512ef74193cd24df48c3dbf15c619c80cb
22dylan <sandersondylan@gmail.com>
2019-10-18 16:44:13 -0700

    changes to plotting

--- I1/code/plotting_results.py
+++ I1/code/plotting_results.py
@@ -2,71 +2,50 @@ import os
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('TkAgg')
 
-
-path_to_output = os.path.join(os.getcwd(), '..', 'output')				# path to output
-part_1_pickle = os.path.join(path_to_output, 'results_p1.pickle')
-part_2_pickle = os.path.join(path_to_output, 'results_p2.pickle')
-part_3_pickle = os.path.join(path_to_output, 'results_p3.pickle')
 
 
 # --- part 1 ---
-results = pickle.load(open(part_1_pickle, "rb" ))
+
+def plot_1a(keys):
+	"""
+	trial_0 -> 10**0
+	trial_1 -> 10**-1
+	trial_2 -> 10**-2
+	trial_3 -> 10**-3
+	trial_4 -> 10**-4
+	trial_5 -> 10**-5
+	trial_6 -> 10**-6
+	trial_7 -> 10**-7
+	"""
+
+	path_to_output = os.path.join(os.getcwd(), '..', 'output')				# path to output
+	part_1_pickle = os.path.join(path_to_output, 'results_p1.pickle')
+
+	fig, ax = plt.subplots(1, 1, figsize=(12,8))
+	ax = np.array(ax)
+	for key in keys:
+		data = pickle.load(open(part_1_pickle, "rb" ))[key]
+
+		label_val = 'learning_rate: {}' .format(data['step_size'])
+		ax.plot(data['iteration'], data['SSE'], label='learning_rate: {}')
+		ax.grid(which='minor', alpha=0.25, color = 'k', ls = ':')
+		ax.grid(which='major', alpha=0.40, color = 'k', ls = '--')
+		ax.set_xscale('log')
+
+		print(data.keys())
+
+	ax.set_ylabel('SSE')
+	ax.set_xlabel('Iteration')
+	# ax.set_xscale('log')
 
 
-learning_rates = [results[i]['step_size'] for i in results.keys()]
-SSE = [results[i]['SSE'] for i in results.keys()]
-convergence_count = [results[i]['convergence_count'] for i in results.keys()]
-print(results['trial_0'])
+def plot_1b():
+	pass
 
-fig, ax = plt.subplots(2, 1, figsize=(12,8))
-ax = np.array(ax)
-
-ax[0].plot(learning_rates, SSE, color='k', lw=2, ls = '-')
-ax[1].plot(learning_rates, convergence_count, color='k', lw=2, ls = '-')
-
-
-ax[0].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-ax[0].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-ax[1].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-ax[1].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-
-ax[0].set_ylabel('SSE')
-ax[1].set_ylabel('Convergence Count')
-ax[1].set_xlabel('Learning Rate')
-
-ax[0].set_xscale('log')
-ax[1].set_xscale('log')
-
-
-
-
-
-# # --- part 2 ---
-# results = pickle.load(open(part_2_pickle, "rb" ))
-# learning_rates = [results[i]['step_size'] for i in results.keys()]
-# SSE = [results[i]['SSE'] for i in results.keys()]
-# convergence_count = [results[i]['convergence_count'] for i in results.keys()]
-
-# fig, ax = plt.subplots(2, 1, figsize=(12,8))
-# ax = np.array(ax)
-
-# ax[0].plot(learning_rates, SSE, color='k', lw=2, ls = '-')
-# ax[1].plot(learning_rates, convergence_count, color='k', lw=2, ls = '-')
-
-
-# ax[0].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-# ax[0].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-# ax[1].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-# ax[1].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-
-# ax[0].set_ylabel('SSE')
-# ax[1].set_ylabel('Convergence Count')
-# ax[1].set_xlabel('Learning Rate')
-
-# ax[0].set_xscale('log')
-# ax[1].set_xscale('log')
-# plt.show()
+plot_1a(['trial_5', 'trial_6', 'trial_7'])		# trials 5, 6, and 7 don't diverge
 
 
 
