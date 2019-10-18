e172ec5295b6e584767e2f5e2f3c6836d938a8f5
jacksder <32549212+jacksder@users.noreply.github.com>
2019-10-18 14:33:33 -0700

    commmit

--- I1/code/BatchGradientDescent.py
+++ I1/code/BatchGradientDescent.py
@@ -6,8 +6,10 @@ import csv
 # test = np.full((50,4),.5)
 
 def bgd(lambda_reg, stepSize, data):
-    iterations = 0
     
+    iterations = 0
+    SSE_results = []
+
     #initialize w to number of parameters, ignoring cost column
     params_len = data[0].size - 1
     w = np.zeros(params_len) #initial guess 
@@ -31,6 +33,12 @@ def bgd(lambda_reg, stepSize, data):
         w = w - stepSize*loss_gradient
 
         iterations = iterations + 1
+
+        #save sum square error every 100 iterations
+        if (iterations%100 == 0):
+            SSE_results.append([iterations, np.linalg.norm(sumErr)])
+
+        #print info to know its working
         if (iterations%1000 == 0):
             print("Done with iteration {0}".format(iterations))
             print("|loss_gradient|: {0}".format(np.linalg.norm(loss_gradient)))
@@ -43,11 +51,12 @@ def bgd(lambda_reg, stepSize, data):
             # input()
         
         #stop if diverging
-        if (np.linalg.norm(loss_gradient) > 10**10):
+        if (np.linalg.norm(loss_gradient) > 10**30):
             break
 
     print("Regression complete with iteration: {0}".format(iterations))
     print("|loss_gradient|: {0}".format(np.linalg.norm(loss_gradient)))
+    print("SSE_results: {0}".format(SSE_results))
     print("w: {0}".format(w))
     # print("x_i: {0}".format(x_i))
     print("y_i: {0}".format(y_i))
@@ -55,7 +64,7 @@ def bgd(lambda_reg, stepSize, data):
     # print("sumErr: {0}".format(sumErr))
     print("")
     # input()
-    return w, iterations, sumErr
+    return w, iterations, SSE_results
 
 # bgd(0, .01, test)
 
--- I1/code/plotting_results.py
+++ I1/code/plotting_results.py
@@ -1,81 +1,3 @@
-<<<<<<< HEAD
-import os
-import numpy as np
-import pickle
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('TkAgg')
-
-
-
-path_to_output = os.path.join(os.getcwd(), '..', 'output')				# path to output
-part_1_pickle = os.path.join(path_to_output, 'results_p1.pickle')
-part_2_pickle = os.path.join(path_to_output, 'results_p2.pickle')
-part_3_pickle = os.path.join(path_to_output, 'results_p3.pickle')
-
-
-# --- part 1 ---
-results = pickle.load(open(part_1_pickle, "rb" ))
-
-
-learning_rates = [results[i]['step_size'] for i in results.keys()]
-SSE = [results[i]['SSE'] for i in results.keys()]
-convergence_count = [results[i]['convergence_count'] for i in results.keys()]
-print(results['trial_2'])
-
-fig, ax = plt.subplots(2, 1, figsize=(12,8))
-ax = np.array(ax)
-
-ax[0].plot(learning_rates, SSE, color='k', lw=2, ls = '-')
-ax[1].plot(learning_rates, convergence_count, color='k', lw=2, ls = '-')
-
-
-ax[0].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-ax[0].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-ax[1].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-ax[1].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-
-ax[0].set_ylabel('SSE')
-ax[1].set_ylabel('Convergence Count')
-ax[1].set_xlabel('Learning Rate')
-
-ax[0].set_xscale('log')
-ax[1].set_xscale('log')
-
-
-
-
-
-# # --- part 2 ---
-# results = pickle.load(open(part_2_pickle, "rb" ))
-# learning_rates = [results[i]['step_size'] for i in results.keys()]
-# SSE = [results[i]['SSE'] for i in results.keys()]
-# convergence_count = [results[i]['convergence_count'] for i in results.keys()]
-
-# fig, ax = plt.subplots(2, 1, figsize=(12,8))
-# ax = np.array(ax)
-
-# ax[0].plot(learning_rates, SSE, color='k', lw=2, ls = '-')
-# ax[1].plot(learning_rates, convergence_count, color='k', lw=2, ls = '-')
-
-
-# ax[0].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-# ax[0].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-# ax[1].grid(which='minor', alpha=0.25, color = 'k', ls = ':')
-# ax[1].grid(which='major', alpha=0.40, color = 'k', ls = '--')
-
-# ax[0].set_ylabel('SSE')
-# ax[1].set_ylabel('Convergence Count')
-# ax[1].set_xlabel('Learning Rate')
-
-# ax[0].set_xscale('log')
-# ax[1].set_xscale('log')
-# plt.show()
-
-
-
-
-=======
 import os
 import numpy as np
 import pickle
@@ -149,5 +71,4 @@ ax[1].set_xscale('log')
 
 
 
->>>>>>> 3c75138316602a5d768a1f8806cde052f55888b9
 plt.show()
