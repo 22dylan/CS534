bb6d5e701590a37fb3db995e89e50b42f9d1ecdb
22dylan <sandersondylan@gmail.com>
2019-10-19 17:26:15 -0700

    part2

--- I1/code/plotting_results.py
+++ I1/code/plotting_results.py
@@ -25,14 +25,13 @@ def plot_SSE_v_Iterations(pickle_str, keys):
 
 
 	path_to_pickle = os.path.join(os.getcwd(), '..', 'output', pickle_str)				# path to output
-
 	data = pickle.load(open(path_to_pickle, "rb" ))
 
 	num_keys = len(keys)
 	fig, ax = plt.subplots(1, 1, figsize=(12,8))
 	labels=[]
-	colors = ['k', 'r', 'b']
-	linestyle = ['-', ':', '-.', '-', ':', '-.', '-', ':', '-.']
+
+	linestyle = ['-', ':', '-.', '-']
 	markers = ['.', '<', '^', 'v', '>', '+', 's']
 
 	for i in range(num_keys):
@@ -46,29 +45,59 @@ def plot_SSE_v_Iterations(pickle_str, keys):
 			if i == 0:
 				labels.append(r'$\lambda$= {}' .format(data_temp['lambda_reg']))
 
-			ax.plot(iteration, SSE, color=colors[i], marker=markers[count], markevery=0.1, ls=linestyle[count])
-
+			ax.plot(iteration, SSE, color='k', markevery=0.1, ls='-', marker=markers[count], linewidth=0.75)
 			ax.grid(which='minor', alpha=0.25, color = 'k', ls = ':')
 			ax.grid(which='major', alpha=0.40, color = 'k', ls = '--')
 			count += 1
-
-
-
 
 		ax.set_title('Training SSE')
 		ax.set_ylabel('SSE')
 		ax.set_xscale('log')
 		ax.set_yscale('log')
+
 	ax.set_xlabel('Iteration')
 	plt.subplots_adjust(bottom=0.15)
 
-	black_patch = mpatches.Patch(color='k', label=r'Learning Rate: $10^{-5}$')
-	red_patch = mpatches.Patch(color='r', label=r'Learning Rate: $10^{-6}$')
-	blue_patch = mpatches.Patch(color='b', label=r'Learning Rate: $10^{-7}$')
-	ax.legend(handles=[black_patch, red_patch, blue_patch])
+	# black_patch = mpatches.Patch(color='k', label=r'Learning Rate: $10^{-5}$')
+	# red_patch = mpatches.Patch(color='r', label=r'Learning Rate: $10^{-6}$')
+	# blue_patch = mpatches.Patch(color='b', label=r'Learning Rate: $10^{-7}$')
+	# ax.legend(handles=[black_patch, red_patch, blue_patch])
 
 
 	fig.legend(labels = labels, loc="lower center", ncol=4)
+
+def plot_w_values(pickle_str, keys):
+	path_to_pickle = os.path.join(os.getcwd(), '..', 'output', pickle_str)				# path to output
+	data = pickle.load(open(path_to_pickle, "rb" ))
+
+	fig, ax = plt.subplots(1, 1, figsize=(12,8))
+	w_str = ['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront',\
+	  		  'view','condition','grade','sqft_above','sqft_basement','yr_built',\
+              'yr_renovated','zipcode', 'lat','long','sqft_living15','sqft_lot15',\
+              'year', 'month', 'day']
+
+
+	linestyle = ['-', ':', '-.', '-']
+	markers = ['.', '<', '^', 'v', '>', '+', 's']
+	for i, key in enumerate(keys):
+		data_temp = data[key]
+
+		w = data_temp['w_values']
+		label_str = r'$\lambda: ${}' .format(data_temp['lambda_reg'])
+
+		ax.plot(w, ls='-', marker=markers[i], color='k', label=label_str, linewidth=0.75)
+
+	ax.grid(which='minor', alpha=0.25, color = 'k', ls = ':')
+	ax.grid(which='major', alpha=0.40, color = 'k', ls = '--')
+	ax.set_xticks(range(len(w_str)))
+	ax.set_xticklabels(w_str, rotation=40, ha='right')
+	
+	# ax.set_yscale('log')
+	fig.legend(loc="lower center", ncol=4)
+	plt.subplots_adjust(bottom=0.2)
+
+	ax.set_ylabel('w')
+
 
 def view_W_values(pickle_str, keys):
 	""" prints results contrained in each trial
@@ -84,41 +113,47 @@ def view_W_values(pickle_str, keys):
 		print('\tstep_size: {}' .format(data_temp['step_size']))
 		print('\tlambda_reg: {}' .format(data_temp['lambda_reg']))
 		print('\tw_values: {}' .format(data_temp['w_values']))	
-	pass
+		print(len(data_temp['w_values']))
+	print('\n')
 
 
 def view_validation_results(pickle_str, keys):
 	path_to_pickle = os.path.join(os.getcwd(), '..', 'output', pickle_str)
 	data = pickle.load(open(path_to_pickle, "rb" ))
-	print(data.keys())
 	for i in range(len(keys)):
 		for key in keys[i]:
 			data_temp = data[key]
 			print(np.linalg.norm(data_temp))
-
+	print('\n')
 
 def view_training_SSE_results(pickle_str, keys):
 	path_to_pickle = os.path.join(os.getcwd(), '..', 'output', pickle_str)
 	data = pickle.load(open(path_to_pickle, "rb" ))
-	print(data.keys())
 	SSE_ret = []
 	for i in range(len(keys)):
 		for key in keys[i]:
-			print(data[key].keys())
 			SSE = data[key]['SSE'][-1][1]
 			SSE_ret.append(np.linalg.norm(SSE))
 			print(SSE_ret[-1])
-
+	print('\n')
 
 trials = [
-			['trial_0', 'trial_1', 'trial_2', 'trial_3', 'trial_4', 'trial_5', 'trial_6', 'trial_7']
+			['trial_0', 'trial_1', 'trial_2', 'trial_3', 'trial_4', 'trial_5', 'trial_6']
 		 ]
 
+# trials = [
+# 			['trial_35', 'trial_36', 'trial_37']
+# 		 ]
 
-# plot_SSE_v_Iterations(pickle_str='results_p1_drs.pickle', keys=trials)
-# view_W_values(pickle_str = 'results_p2.pickle', keys=['trial_41', 'trial_42'])
-view_validation_results(pickle_str='results_validation_p1.pickle', keys=[trials[0]])
-# view_training_SSE_results(pickle_str='results_p1.pickle', keys=[trials[0]])
+
+
+
+# plot_SSE_v_Iterations(pickle_str='results_p2.pickle', keys=trials)
+# plot_w_values(pickle_str = 'results_p2.pickle', keys=trials[0])
+
+# view_W_values(pickle_str = 'OLD_results_p2.pickle', keys=['trial_6'])
+view_validation_results(pickle_str='results_validation_p2.pickle', keys=[trials[0]])
+view_training_SSE_results(pickle_str='results_p2.pickle', keys=[trials[0]])
 
 
 plt.show()
