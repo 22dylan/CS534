cb35e6b08d035f4d44d00a3a2bdfd1b072560ca6
jacksder <32549212+jacksder@users.noreply.github.com>
2019-11-01 11:08:36 -0700

    p3 plots and kplabel

--- I2/code/part3_TrainModel.py
+++ I2/code/part3_TrainModel.py
@@ -117,7 +117,7 @@ def runPerceptrons():
     np.savetxt(path_to_output, acc_val_all, delimiter=',')
 
 def plotData():
-    #get accuracy
+    #get accuracies
     colors = ['b','r','g','m','k']
     path_to_acc_train = os.path.join('..', 'output', 'acc_train_p3.csv')
     acc_train_all = np.genfromtxt(path_to_acc_train, delimiter =',')
@@ -125,23 +125,44 @@ def plotData():
     path_to_acc_val = os.path.join('..', 'output', 'acc_val_p3.csv')
     acc_val_all = np.genfromtxt(path_to_acc_val, delimiter =',')
 
+    #plotting accuracy vs iterations for all p
     iterations = np.linspace(1, 15, 15)
 
     fig, ax = plt.subplots(1,1, figsize=(8, 6))
     p = 0
     for acc_train in acc_train_all:
-        ax.plot(iterations, acc_train, color=colors[p], ls='-', label = 'Training, p={0}'.format(p+1))
+        ax.plot(iterations, acc_train, color=colors[p], ls='-', label = 'p={0}, Training'.format(p+1))
         p += 1
     p = 0
     for acc_val in acc_val_all:
-        ax.plot(iterations, acc_val, color=colors[p], ls='-.', label = 'Validation, p={0}'.format(p+1))
+        ax.plot(iterations, acc_val, color=colors[p], ls='-.', label = ' p={0}, Validation'.format(p+1))
         p += 1
 
-    ax.legend()
+    ax.legend(ncol=2)
     ax.grid(which='minor', alpha=0.25, color = 'k', ls = ':')
     ax.grid(which='major', alpha=0.40, color = 'k', ls = '--')
+    plt.xlabel('Iteration')
+    plt.ylabel('Accuracy (%)')
     plt.show()
+
+    #plotting p vs accuracy
+    # points_to_plot = [ acc_val_all[0][3], acc_val_all[1][7], acc_val_all[2][3], acc_val_all[3][6], acc_val_all[4][5] ]
+    # iterations2 = np.linspace(1, 5, 5)
+    # fig2, ax2 = plt.subplots(1,1, figsize=(8, 6))
+    # ax2.plot(iterations2, points_to_plot, color=colors[0], ls='-',marker='o')
+    # # ax2.plot(iterations2, acc_val_all[0][3], color=colors[0], ls='-', label = 'p=1, iter=4')
+    # # ax2.plot(iterations2, acc_val_all[1][7], color=colors[1], ls='-', label = 'p=2, iter=8')
+    # # ax2.plot(iterations2, acc_val_all[2][3], color=colors[2], ls='-', label = 'p=3, iter=4')
+    # # ax2.plot(iterations2, acc_val_all[3][6], color=colors[3], ls='-', label = 'p=4, iter=7')
+    # # ax2.plot(iterations2, acc_val_all[4][5], color=colors[4], ls='-', label = 'p=5, iter=6')
+
+    # ax2.legend()
+    # ax2.grid(which='minor', alpha=0.25, color = 'k', ls = ':')
+    # ax2.grid(which='major', alpha=0.40, color = 'k', ls = '--')
+    # plt.xlabel('p')
+    # plt.ylabel('Accuracy (%)')
+    # plt.show()
 
 
 # runPerceptrons()
-plotData()
+# plotData()
