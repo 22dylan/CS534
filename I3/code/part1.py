665ef7f4530de1bf267dd8085e9def76c1184c7a
22dylan <sandersondylan@gmail.com>
2019-11-21 17:10:29 -0800

    addressing some minor fixes

--- I3/code/part1.py
+++ I3/code/part1.py
@@ -27,8 +27,11 @@ y = data0.columns[-1]
 # features = column_names[:-1]
 # y = column_names[-1]
 
-# --- running code ---
-depth = 4
+
+
+# --- Part 1a ---
+# create a tree with a maximum depth of 2
+depth = 5
 
 """  
 creating tree by predefining all possible paths 
@@ -42,7 +45,6 @@ creating tree by predefining all possible paths
 	  		    etcetera
 	  		      etc.
 """
-
 c0 = len(data0.loc[data0[y] == 0])
 c1 = len(data0.loc[data0[y] == 1])
 
@@ -54,6 +56,7 @@ tree['root']['f=1'] = c1
 tree['root']['prob'] = 1
 tree['root']['U'] = 1 - (c1/(c1+c0))**2 - (c0/(c1+c0))**2				#note: want to confirm this.
 tree['root']['continue'] = True
+tree['root']['split_on'] = None
 
 for i in range(depth):
 	temp = list(itertools.product([1,0], repeat=i+1))
@@ -65,27 +68,64 @@ for i in range(depth):
 	for ii in temp:
 		tree[ii] = {}
 
-
 for i in range(1,depth+1):
 	if (len(features)) > 0:
 		tree, features = HF.find_best_feature(tree, layer=i, features=features, y=y)
 
 tree = {i:j for i,j in tree.items() if j != {}}	# removing empty keys
-print(tree.keys())
-print(len(tree.keys()))
+
+path_to_outfile = os.path.join(os.getcwd(), '..', 'output', 'part1', 'part1a_D{}.csv' .format(depth))
+HF.write_out_tree(tree, path_to_outfile)
 
 
-"""
-TO-DO: write out the trained tree and splits in a way that will be easy to validate/test model on
 
-node, feature
-----  -------
-root    x2
-1	    x1
-0		x1
-1-1		x5
-1-0		x5
-0-1		x5
-0-0		x5
-etc.
-"""
+
+
+
+# # --- Part 1c ---
+# # create a tree with a maximum depth ranging from 1-8
+# depth_vals = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# for depth in depth_vals:
+# 	print(depth)
+
+# 	# re-reading data for a fresh start
+# 	path_to_data = os.path.join(os.getcwd(), '..', 'data', 'pa3_train.csv')
+# 	data0 = HF.datareader(path_to_data)
+# 	features= list(data0.columns[:-1])
+# 	y = data0.columns[-1]
+
+# 	c0 = len(data0.loc[data0[y] == 0])
+# 	c1 = len(data0.loc[data0[y] == 1])
+
+# 	tree = {}
+# 	tree['root'] = {}
+# 	tree['root']['data'] = data0
+# 	tree['root']['f=0'] = c0
+# 	tree['root']['f=1'] = c1
+# 	tree['root']['prob'] = 1
+# 	tree['root']['U'] = 1 - (c1/(c1+c0))**2 - (c0/(c1+c0))**2				#note: want to confirm this.
+# 	tree['root']['continue'] = True
+# 	tree['root']['split_on'] = None
+
+# 	for i in range(depth):
+# 		temp = list(itertools.product([1,0], repeat=i+1))
+
+# 		if len(temp[0]) == 1:
+# 			temp = [str(i[0]) for i in temp]
+# 		else:
+# 			temp = ['-'.join(map(str, i)) for i in temp]
+# 		for ii in temp:
+# 			tree[ii] = {}
+
+# 	for i in range(1,depth+1):
+# 		if (len(features)) > 0:
+# 			tree, features = HF.find_best_feature(tree, layer=i, features=features, y=y)
+
+# 	tree = {i:j for i,j in tree.items() if j != {}}	# removing empty keys
+
+# 	path_to_outfile = os.path.join(os.getcwd(), '..', 'output', 'part1', 'part1c_D{}.csv' .format(depth))
+# 	HF.write_out_tree(tree, path_to_outfile)
+
+
+
